7583a72da40568db0c436d14cf84ae9125633285
Julian Achatzi <julian.achatzi@fh-koeln.de>
2015-01-29 18:58:36 +0100

    changed bitwise and to bitwise or in firmware/MCP413C.cpp

--- firmware/MCP413X.cpp
+++ firmware/MCP413X.cpp
@@ -13,12 +13,12 @@ void MCP413X::begin() {
 
 void MCP413X::decrease(uint8_t wiper) {
 	// Write combined bit masks of wiper address and decrease command
-	write(wiper &= COMMAND_INCREASE);
+	write(wiper |= COMMAND_INCREASE);
 }
 
 void MCP413X::increase(uint8_t wiper) {
 	// Write combined bit masks of wiper address and increase command
-	write(wiper &= COMMAND_DECREASE);
+	write(wiper |= COMMAND_DECREASE);
 }
 
 void MCP413X::write(uint8_t message) {
